e75980a5fbca76ffa308f66239b80d0b9eed4a3d
Spoofing <spoofing@spfng.com>
2015-06-11 12:38:53 +0700

    first commit

--- index.php
+++ index.php
@@ -325,7 +325,6 @@ Thread ID not found.
 Password <input type="password" name="password"> [<input type="checkbox" name="fileonly"> File only] <input type="submit" value="Delete">
 </form>
 </div>
-<!-- Yandex.Metrika counter --><script type="text/javascript">(function (d, w, c) { (w[c] = w[c] || []).push(function() { try { w.yaCounter30410527 = new Ya.Metrika({id:30410527, trackLinks:true, accurateTrackBounce:true}); } catch(e) { } }); var n = d.getElementsByTagName("script")[0], s = d.createElement("script"), f = function () { n.parentNode.insertBefore(s, n); }; s.type = "text/javascript"; s.async = true; s.src = (d.location.protocol == "https:" ? "https:" : "http:") + "//mc.yandex.ru/metrika/watch.js"; if (w.opera == "[object Opera]") { d.addEventListener("DOMContentLoaded", f, false); } else { f(); } })(document, window, "yandex_metrika_callbacks");</script><noscript><div><img src="//mc.yandex.ru/watch/30410527" style="position:absolute; left:-9999px;" alt="" /></div></noscript><!-- /Yandex.Metrika counter -->
 </body>
 </html>
 <?php endif; ?>
